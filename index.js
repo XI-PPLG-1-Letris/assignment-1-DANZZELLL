91ee3d7fc8a53618391ecd98a91828d36772e1df
DANZZELLL <rafazaidan07@gmail.com>
2025-10-05 19:30:34 +0700

    done pak

---
+++ index.js
@@ -0,0 +1,37 @@
+a = 3
+b = 2
+const perkalian = () => {
+console.log(`perkalian ${a} dikali ${b} adalah ${a * b}`);
+
+}
+perkalian()
+
+const tambah = () => {
+console.log(`penjumlahan ${a} dijumlah ${b} adalah ${a + b}`);
+
+}
+tambah()
+
+const bagi = () => {
+console.log(`pembagian ${a} dibagi ${b} adalah ${a / b}`);
+
+}
+bagi()
+
+const kurang = () => {
+console.log(`pengurangan ${a} dikurang ${b} adalah ${a - b}`);
+
+}
+kurang()
+
+const pangkat = () => {
+console.log(`perpangkatan ${a} dipangkat ${b} adalah ${a ** b}`);
+
+}
+pangkat()
+
+const modulus = () => {
+console.log(`modulus ${a} dimodulus ${b} adalah ${a % b}`);
+
+}
+modulus()
